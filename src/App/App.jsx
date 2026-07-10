673a5d549038e4b58c21695d5277d76a75b3103d
hamideh-b <hamideh.b120@gmail.com>
2026-07-10 12:39:56 +0330

    Fix import path for deployment-1

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -17,7 +17,7 @@ import CartSideBar from "../Components/CartSideBar/CartSideBar";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import Search from "../Pages/Search/Search";
-import ProdutPage from "../Pages/ProdutPage/produtPage";
+import ProdutPage from "../Pages/ProdutPage/ProdutPage";
 
 const App = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
@@ -36,7 +36,7 @@ const App = () => {
           <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/shop" element={<Shop />} />
           <Route path="/shop/:category" element={<Shop />} />
-          <Route path="/shop/:category/:slug" element={<ProdutPage />} />
+          <Route path="/shop/:category/:slug" element={<Pro />} />
           <Route path="/search" element={<Search />} />
         </Routes>
         <BottomNav />
